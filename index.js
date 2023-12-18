d657ac2093ea85e31cd419aa9355c4325b643dbc
Ryan <griffinryan77@gmail.com>
2023-12-18 18:06:00 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution in this file!
+const employee = {
+    Key: {"name": "Ray",
+    "streetAddress": "12k street"}
+    };
+
+function updateEmployeeWithKeyAndValue(){
+    const emp = { ... employee};
+    emp["name"] = "Sam";
+    emp["streetAddress"] = "11 Broadway";
+    return emp
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(){
+    const kek = employee
+    kek["name"] = "Sam";
+    kek.streetAddress = "12 Broadway";
+    return kek
+}
+
+function deleteFromEmployeeByKey(){
+    const kekw = {...employee};
+    delete kekw.name;
+    kekw[name] = "Ryan";
+    return kekw
+}
+
+function destructivelyDeleteFromEmployeeByKey(){
+    const pie = employee
+    delete pie.name;
+    pie[name] = "Sam";
+    return pie
+}
+
+console.log(updateEmployeeWithKeyAndValue)
+console.log(destructivelyUpdateEmployeeWithKeyAndValue)
+console.log(deleteFromEmployeeByKey)
+console.log(destructivelyDeleteFromEmployeeByKey)
